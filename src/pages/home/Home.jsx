aaf00047dc0e88bcad06ca920eb53ee46686dd76
Roshan <houseno14a@gmail.com>
2023-12-01 14:58:54 +0530

    completed home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroBanner from './heroBanner/HeroBanner'
+import "./style.scss"
+import Trending from './trending/Trending'
+const Home = () => {
+  return (
+    <div className='homePage'>
+        <HeroBanner/>
+        <Trending/>
+    </div>
+  )
+}
+
+export default Home
